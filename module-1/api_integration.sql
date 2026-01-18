1aa8b3c944dc328ebc48894813e7965f6864761b
huealu <huehong.dinh@gmail.com>
2026-01-18 08:15:13 -0600

    updated setup

--- module-1/api_integration.sql
+++ module-1/api_integration.sql
@@ -18,17 +18,19 @@ CREATE OR REPLACE SECRET course_repo.public.github_pat
 
 
 -- Create the API integration
-CREATE OR REPLACE API INTEGRATION
+CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('') -- URL to your GitHub profile
-  ALLOWED_AUTHENTICATION_SECRETS = ()
+  API_ALLOWED_PREFIXES = ('https://github.com/huealu') -- URL to your GitHub profile
+  -- The line above will allow the access to all repositories under the specified GitHub repo URL.
+  ALLOWED_AUTHENTICATION_SECRETS = (github_pat)
   ENABLED = TRUE;
 
 -- Create the git repository object
 CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_snowflake
-  API_INTEGRATION =  -- Name of the API integration defined above
+  API_INTEGRATION =  git_api_integration -- Name of the API integration defined above
   ORIGIN = '' -- Insert URL of forked repo
-  GIT_CREDENTIALS = ;
+  -- The line above: spcify the path to the GitHub repo that you forked. 
+  GIT_CREDENTIALS = course_repo.public.github_pat;
 
 -- List the git repositories
 SHOW GIT REPOSITORIES;
